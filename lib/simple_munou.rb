cabb869002eeb114648dd1fba47610f53b680b72
esehara shigeo <esehara@gmail.com>
2015-04-22 11:31:37 +0900

    add: prototype coding

--- lib/simple_munou.rb
+++ lib/simple_munou.rb
@@ -1,5 +1,71 @@
 require "simple_munou/version"
 
+class Array
+  def to_munou
+    SimpleMunou::Brain.new self
+  end
+end
+
 module SimpleMunou
-  # Your code goes here...
+  class Brain
+    attr_reader :words
+    def initialize(talk_pattern)
+      @pattern = talk_pattern
+    end
+
+    def words=(words)
+      @words = words.map do |elem|
+        word = case elem
+               when String then
+                 SimpleMunou::Word.new(elem, 0)
+               when Array then        
+                 if (elem.size == 2) && (valid_word_array?(elem))
+                   SimpleMunou::Word.new(elem[0], elem[1])
+                 end
+               when SimpleMunou::Word
+                 elem
+               end
+        if word.nil?
+          fail TypeError, "Not Support Type" 
+        end
+        word
+      end
+    end
+
+    def say
+      @pattern.map do |p|
+        case p
+        when Fixnum
+          choice_words p
+        when String
+          p
+        end
+      end.join
+    end
+
+    def valid_words(kind_num)
+      kind_num > 0 ? @words.select {|word| word.kind == kind_num} : @words
+    end
+    
+    private
+    def choice_words(kind_num)
+      valid_words(kind_num).sample
+    end
+
+    def valid_word_array?(elem)
+      (elem[0].class == String) and (elem[1].class == Fixnum)
+    end
+  end
+
+  class Word
+    attr_reader :kind
+    def initialize(word, kind=0)
+      @kind = kind
+      @word = word
+    end
+
+    def to_s
+      @word
+    end
+  end
 end
--- spec/simple_munou_spec.rb
+++ spec/simple_munou_spec.rb
@@ -5,7 +5,43 @@ describe SimpleMunou do
     expect(SimpleMunou::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can array to munou pattern' do
+    expect([0, "is foobar"].to_munou.class).to be SimpleMunou::Brain
+  end
+
+  it 'can set word list to munou pattern' do
+    munou = [0, "is foobar"].to_munou
+    munou.words = ["foo", "bar"]
+    expect(munou.words.size).to eq 2
+  end
+
+  it 'can set words if string' do
+    munou = [0, "is foobar"].to_munou
+    munou.words = ["foo", "bar"]
+    expect(munou.words[0].class).to be SimpleMunou::Word
+  end
+
+  it 'can generate words' do
+    munou = [0, " is foobar"].to_munou
+    munou.words = ["foo"]
+    expect(munou.say).to eq "foo is foobar" 
+  end
+
+  it 'set kind if array and 2 size' do
+    munou = [1, " is foobar"].to_munou
+    munou.words = [["foo", 1]]
+    expect(munou.words[0].kind).to eq 1
+  end
+
+  it 'choice same kind from word list' do
+    munou = [1, " is foobar"].to_munou
+    munou.words = [["foo", 1], ["bar", 2]]
+    expect(munou.say).to eq "foo is foobar"
+  end
+
+  it 'choice all word if word kind is -1'do
+    munou = [-1, " is foobar"].to_munou
+    munou.words = [["foo", 1], ["bar", 2]]
+    expect(munou.valid_words(-1).size).to eq 2
   end
 end
